a10651d12ec71aba4bc7cbd54a0bfb8b64aa016d
Luv Gangwani <luv.gangwani@gmail.com>
2021-02-24 00:26:56 +1100

    Lesson 17 - Custom fonts

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar, View } from 'react-native';
+import * as Font from 'expo-font';
 import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () =>
+  Font.loadAsync({
+    'raleway-light': require('./assets/fonts/Raleway-Light.ttf'),
+    'raleway-semibold': require('./assets/fonts/Raleway-SemiBold.ttf')
+  });
 
 export default function App() {
-  return (
-    <View>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  let returnElement = <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} onError={console.warn} />;
+
+  if (fontsLoaded) 
+    returnElement = <View>
+                      <Home />
+                      <StatusBar style="auto" />
+                    </View>
+  return returnElement;
 }

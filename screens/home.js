e9e772061f13f6a95430978292b1b37ac35af6e4
Luv Gangwani <luv.gangwani@gmail.com>
2021-02-28 23:56:00 +1100

    Lesson 21 - Passing data between two screens

--- screens/home.js
+++ screens/home.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import globalStyles from '../styles/global';
 
 export default function Home({ navigation }) {
 
-    const pressHandler = () => navigation.navigate('Review Details');
+    const [reviews, setReviews] = useState([
+        {title: 'Fast and Furious' , rating: '2', body: 'Lorem ipsum', key: '1'},
+        {title: '2 Fast 2 Furious' , rating: '5', body: 'Lorem ipsum', key: '2'},
+        {title: 'Fast and Furious: Tokyo Drift' , rating: '3', body: 'Lorem ipsum', key: '3'},
+        {title: 'Fast and Furious 4' , rating: '4', body: 'Lorem ipsum', key: '4'},
+    ]);
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home screen</Text>
-            <Button title="Go to review details" onPress={pressHandler} />
+            <FlatList
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Review Details', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     );
 };
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Button, Text, View } from 'react-native';
 import globalStyles from '../styles/global';
 
-export default function ReviewDetails({ navigation }) {
-    
-    const goBackHome = () => navigation.goBack();
-
+export default function ReviewDetails({ route, navigation }) {
+    const { title, rating, body } = route.params;
     return (
         <View style={globalStyles.container}>
-            <Text>Review details screen</Text>
-            <Button title='Go back home' onPress={goBackHome} />
+            <Text>Title: { title }</Text>
+            <Text>Rating: { rating }</Text>
+            <Text>Body: { body }</Text>
         </View>
     );
 };

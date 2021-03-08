44383566c4362a52e28794f13a047e17eb0680be
Luv Gangwani <luv.gangwani@gmail.com>
2021-03-09 00:08:01 +1100

    Lesson 27 - Using images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import Card from '../shared/card';
-import globalStyles from '../styles/global';
+import globalStyles, { images } from '../styles/global';
 
 export default function ReviewDetails({ route, navigation }) {
     const { title, rating, body } = route.params;
@@ -9,9 +9,23 @@ export default function ReviewDetails({ route, navigation }) {
         <View style={globalStyles.container}>
             <Card>
                 <Text>Title: { title }</Text>
-                <Text>Rating: { rating }</Text>
                 <Text>Body: { body }</Text>
+                <View style={styles.rating}>
+                    <Text>Rating:</Text>
+                    <Image source={images.rating[rating]}/>
+                </View>
             </Card>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#184e77',
+    }
+});
--- styles/global.js
+++ styles/global.js
@@ -14,4 +14,14 @@ const globalStyles = StyleSheet.create({
     }
 });
 
+export const images = {
+    rating: {
+        '1': require('../assets/rating-1.png'),
+        '2': require('../assets/rating-2.png'),
+        '3': require('../assets/rating-3.png'),
+        '4': require('../assets/rating-4.png'),
+        '5': require('../assets/rating-5.png'),
+    }
+}
+
 export default globalStyles;
